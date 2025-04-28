82a46b8eac1052378dbbeb9441331837be160f78
renchang.peng <renchang.peng@riversecurity.com>
2025-04-28 17:51:39 +0800

    添加类验证器和转换器，更新用户数据传输对象以进行验证，并在主应用中启用全局验证管道

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
   id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 }
