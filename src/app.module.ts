fe1b8a652190ef0458b6055d852cab38abd596a7
renchang.peng <renchang.peng@riversecurity.com>
2025-04-29 17:03:27 +0800

    在主模块中添加全局异常过滤器，以处理 HTTP 异常

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './database/database.config';
 import { AuthModule } from './auth/auth.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 @Module({
   imports: [TypeOrmModule.forRoot(databaseConfig), UsersModule, AuthModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/filters/http-exception.filter.ts
@@ -0,0 +1,48 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response, Request } from 'express';
+
+interface ExceptionResponse {
+  message: string | string[];
+  error: string;
+}
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message: string = 'Internal server error';
+    let error = 'Internal Server Error';
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const exceptionResponse = exception.getResponse() as
+        | ExceptionResponse
+        | string;
+      message =
+        typeof exceptionResponse === 'string'
+          ? exceptionResponse
+          : Array.isArray(exceptionResponse.message)
+            ? exceptionResponse.message.join(', ')
+            : exceptionResponse.message;
+      error = exception.name;
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+      error,
+    });
+  }
+}
