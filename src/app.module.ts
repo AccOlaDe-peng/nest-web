69c90c5a57d22416d6ced3592752a32d128a41a1
renchang.peng <renchang.peng@riversecurity.com>
2025-04-29 10:41:05 +0800

    添加 Swagger 支持，更新 package.json 和 yarn.lock 文件以引入相关依赖，删除不再使用的 AppController 和 AppService，更新主模块以反映这些更改。

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './database/database.config';
 
 @Module({
   imports: [TypeOrmModule.forRoot(databaseConfig), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 配置
+  const config = new DocumentBuilder()
+    .setTitle('Nest Web API')
+    .setDescription('The Nest Web API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
